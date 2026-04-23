69d9c2626b99891c140953522c87461ab8ce4019
DevCat3 <omarelbakery2008@gmail.com>
2026-04-24 01:02:10 +0300

    Everything

--- device.mk
+++ device.mk
@@ -6,3 +6,24 @@
 #
 
 LOCAL_PATH := device/samsung/gta8
+
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-service \
+    android.hardware.fastboot@1.1-service \
+    android.hardware.fastboot@1.0-impl \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.0-impl \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+    android.hardware.health@2.1-service \
+    hwservicemanager \
+    vndservicemanager
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_SHIPPING_API_LEVEL := 30
+TARGET_OTA_ASSERT_DEVICE := gta8
